8bdedf3b1359912f8b21f4e4fe4f895ba809010c
随风铃动的尛善 <70792727+AlienfromNova@users.noreply.github.com>
2021-02-08 10:37:39 +0800

    Delete 作业/第一期 directory

--- 作业/第一期/004-1.py
+++
@@ -1,4 +0,0 @@
-#004-1
-dayup=1.001**365
-daydown=0.999**365
-print("dayup为{:.3f}daydown为{:.3f}".format(dayup,daydown))
--- 作业/第一期/005.py
+++
@@ -1,4 +0,0 @@
-#005
-week='一二三四五六日'
-weekid=int(input("输入星期id"))
-print("\n今天星期" + week[weekid])
